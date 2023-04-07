f3527ddb4de3e48a66c3dd83f9a87ad10372999a
swapnith <swapnithkondapalli@gmail.com>
2023-04-07 14:20:21 +0530

    server code for mysql in examples

---
+++ examples/server/index.js
@@ -0,0 +1,28 @@
+const bodyParser = require('body-parser')
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const mysql = require('mysql2')
+
+const db = mysql.createConnection({
+    host:"localhost",
+    user:'root',
+    password:'root',
+    database:"LandAllotment",
+})
+
+app.use(cors())
+app.use(express.json())
+app.use(bodyParser.urlencoded({extended:true}))
+
+app.listen(3001,()=>{
+    console.log("server is running in port 3001")
+})
+
+app.get("/approved",(req,res)=>{
+    const dbSel = 'select * from LandAllotment.ApprovedLandApplications;'
+    db.query(dbSel,(error,result)=>{
+        res.send(result)
+    })
+
+})
